239b2b888374eaae05fa60e9c65996fc09dab14c
Yachim <jachym.kohout@gmail.com>
2024-01-20 11:18:18 +0100

    gh-pages setup2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import TwoD from "./pages/TwoD"
 import ThreeD from "./pages/ThreeD"
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="2d" element={<TwoD />} />
         <Route path="3d" element={<ThreeD />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
